a3eae41e275a8331ea573a59793abf991e81d254
Zachary C Luscher <zacharyluscher@gmail.com>
2021-07-10 19:35:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1481,7 +1481,7 @@ function getTextureScale (texture, width, height) {
 }
 
 function scaleByPixelRatio (input) {
-    let pixelRatio = 0.1;
+    let pixelRatio = 1.0;
     return Math.floor(input * pixelRatio);
 }
 
